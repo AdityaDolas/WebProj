99bf15f0da9df262d255f43e82897bb33e699652
Aditya Dolas <dolasadi@gmail.com>
2019-09-06 18:40:01 +0530

    added the eclipse project

---
+++ src/com/aditya/config/DBConnect.java
@@ -0,0 +1,19 @@
+package com.aditya.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	public static Connection getConnect() {
+		Connection con=null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","root");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Connecting to database ...");
+		return con;
+		
+	}
+}
